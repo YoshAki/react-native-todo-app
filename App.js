34232edfe10b8640ef3ed3d6678acb80cf2333ac
yoshyosh81 <yoshiaki.suzuki19@gmail.com>
2020-02-14 20:20:42 +0900

    add native-base

--- App.js
+++ App.js
@@ -1,19 +1,39 @@
 import React from 'react';
 import {StyleSheet, Text, View} from 'react-native'
+import TodoList from './component/TodoList'
 
-export default function App () {
-  return (
-    <View style={styles.container}>
-      <Text>Hello World!</Text>
-    </View>
-  )
-}
+// ローディング待ち
+import { AppLoading } from 'expo';
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center"
+// native-base用モジュール
+import * as Font from "expo-font";
+import { Ionicons } from "@expo/vector-icons";
+
+export default class App extends React.Component {
+  // ロードが終わるまでは「loading...」を表示するため、state「isReady」で制御
+  constructor(props) {
+    super(props);
+    this.state = {
+      isReady: false
+    };
   }
-})
+  // DidMountのタイミングでフォントリソースをメモリ上に読み込み。終わったらisReadyをオン。
+  async componentDidMount() {
+    await Font.loadAsync({
+      Roboto: require("native-base/Fonts/Roboto.ttf"),
+      Roboto_medium: require("native-base/Fonts/Roboto_medium.ttf"),
+      ...Ionicons.font
+    });
+    this.setState({ isReady: true });
+  }
+
+  render() {
+    if (!this.state.isReady) {
+      return <AppLoading />;
+    }
+
+    return (
+      <TodoList />
+    );
+  }
+}
